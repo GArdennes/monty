58172fc367665660205215e84037e7077c0b214d
GArdennes <kevin.afachao@gmail.com>
2023-08-23 11:09:13 -0700

    Task 6 sub

---
+++ task_6.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * op_sub - opcode that finds the difference in the top two elements
+ * @stack: stack_t node
+ * @line_number: file line count
+ */
+void op_sub(stack_t **stack, unsigned int line_number)
+{
+	stack_t *pointer = *stack;
+	int buffer, len;
+
+	if (pointer == NULL)
+		exit(EXIT_FAILURE);
+	for (len = 0; pointer != NULL; len++)
+		pointer = pointer->next;
+
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	pointer = *stack;
+	buffer = pointer->next->n - pointer->n;
+	pointer->next->n = buffer;
+
+	*stack = pointer->next;
+	free(pointer);
+}
